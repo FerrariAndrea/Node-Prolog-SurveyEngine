714dbc74d7d18b0a9253ebe83902d0b5b511a1c3
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-10-26 19:06:02 +0200

    set up server

---
+++ Server/applCode.js
@@ -0,0 +1,98 @@
+/*
+frontend/uniboSupports/applCode
+*/
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');	//see 10.1 of nodeExpressWeb.pdf;
+const favicon = require('express-favicon');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const index = require('./appServer/routes/index');
+
+
+var app = express();
+
+// view engine setup;
+app.set('views', path.join(__dirname, 'appServer', 'views'));
+app.set('view engine', 'ejs');
+
+//create a write stream (in append mode) ;
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'morganLog.log'), { flags: 'a' })
+app.use(logger("short", { stream: accessLogStream }));
+
+//Creates a default route. Overloads app.use('/', index);
+//app.get("/", function(req,res){ res.send("Welcome to frontend Server"); } );
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'appServer', 'favicon.ico')));
+app.use(logger('dev'));				//shows commands, e.g. GET /pi 304 23.123 ms - -;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'jsCode'))); //(***)
+
+//DEFINE THE ROUTES ;
+//app.use('/', index);		 
+
+//Creates a default route for /pi;
+app.get('/info', function (req, res) {
+	res.send('This is the CompilationHelper!')
+});
+
+app.get('/', function (req, res) {
+	res.render("index");
+});
+
+
+/*
+* ====================== CMD ================
+*/
+app.get('/test', function (req, res) {
+	res.send("MY TEST TSG")
+});
+
+/*
+* ====================== REPRESENTATION ================
+*/
+app.use(function (req, res) {
+	console.info("SENDING THE ANSWER " + result + " json:" + req.accepts('josn'));
+	try {
+		console.log("answer> " + result);
+	    /*
+	   if (req.accepts('json')) {
+	       return res.send(result);		//give answer to curl / postman
+	   } else {
+	       return res.render('index' );
+	   };
+	   */
+		return res.render('index');
+	} catch (e) { console.info("SORRY ..." + e); }
+}
+);
+
+//app.use(converter());
+
+/*
+ * ============ ERROR HANDLING =======
+ */
+
+// catch 404 and forward to error handler;
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler;
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page;
+	res.status(err.status || 500);
+	res.render('error');
+});
+module.exports = app;
