f14702dc906fa82d4d547d88dcf5459db11e5502
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-10-31 15:03:06 +0100

    Engine work 100%

--- Server/appServer/prolog/prolog.js
+++ Server/appServer/prolog/prolog.js
@@ -1,20 +1,42 @@
 const swipl = require('swipl');
-
+var fs = require('fs');
+ 
 module.exports ={
 
     assert : function(clause){
-        swipl.call('assert('+clause+')');
+        return swipl.call('assert('+clause+')');
     },
     retract : function(clause){
-        swipl.call('retract('+clause+')');
+        return swipl.call('retract('+clause+')');
     },
     readX: function(roule){
-        const ret = swipl.call('clause('+roule+',_)');
+        const ret = swipl.call('clause('+roule+',_)');        
         if (ret) {
-            return  "${ret.X}";
+            return ret.X;
         } else { 
             return false;      
         }
+    },
+    clear: function(){
+        return swipl.call('retractall(answer(_,_,_,_))');        
+    },
+    load: function(module_name){        
+        var contents = fs.readFileSync('./appServer/prolog/pl/'+module_name, 'utf8');
+        var line =0;
+        contents.split('\n').forEach(function(element) {
+            if(element!==undefined && element[0]!=='%'  && element.length>0){
+               try{
+                if(!swipl.call('assert('+element+')')){
+                    console.log("Fail load module "+ module_name+"["+line+"]: assert("+element+").")
+                  }
+               }catch(Exception){
+                    console.log("Fail load module "+ module_name+"["+line+"]: assert("+element+").")
+               }
+             
+            }
+            line++;
+        });  
+        console.log("FINISH")
     }
     
 }
--- Server/appServer/prolog/prologEngine.js
+++ Server/appServer/prolog/prologEngine.js
@@ -1,9 +1,29 @@
-const swipl = require('swipl');
-
+const {assert,retract,readX,load,clear} = require('./prolog');
 class PorlogEngine{
-    costructor(){
+    constructor(){
         
+       this.Init=function (module_name){
+            clear();
+            if(module_name!==undefined){
+                if(module_name.isArray()){
+                    module_name.array.forEach(element => {
+                        load(element);
+                    });
+                }else{
+                    load(module_name);
+                }
+            }
+       };
+       this.test=function(){
+           var x =readX("answer(1,X,'si',2)");
+            return (x=='Hai un gatto?');
+       };
     }
-}
 
-module.exports PorlogEngine;
+  
+
+};
+
+
+PorlogEngine.Istance = new PorlogEngine();
+module.exports = PorlogEngine;
